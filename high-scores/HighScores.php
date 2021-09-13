5b13931d31e544c1c59c49829db140323f6f2c3d
Andi Rückauer <arueckauer@gmail.com>
2021-09-13 16:37:32 +0200

    Add solution for exercise high-scores

--- high-scores/HighScores.php
+++ high-scores/HighScores.php
@@ -1,33 +1,47 @@
 <?php
 
-/*
- * By adding type hints and enabling strict type checking, code can become
- * easier to read, self-documenting and reduce the number of potential bugs.
- * By default, type declarations are non-strict, which means they will attempt
- * to change the original type to match the type specified by the
- * type-declaration.
- *
- * In other words, if you pass a string to a function requiring a float,
- * it will attempt to convert the string value to a float.
- *
- * To enable strict mode, a single declare directive must be placed at the top
- * of the file.
- * This means that the strictness of typing is configured on a per-file basis.
- * This directive not only affects the type declarations of parameters, but also
- * a function's return type.
- *
- * For more info review the Concept on strict type checking in the PHP track
- * <link>.
- *
- * To disable strict typing, comment out the directive below.
- */
-
 declare(strict_types=1);
+
+namespace Exercism\HighScores;
+
+use function array_slice;
+use function end;
+use function max;
+use function rsort;
 
 class HighScores
 {
+    /** @psalm-var non-empty-array<int, int> */
+    private array $scores;
+
+    /**
+     * @psalm-param non-empty-array<int, int> $bar
+     */
     public function __construct(array $scores)
     {
-        throw new \BadFunctionCallException("Implement the HighScores class");
+        $this->scores = $scores;
+    }
+
+    public function scores(): array
+    {
+        return $this->scores;
+    }
+
+    public function latest(): int
+    {
+        return end($this->scores);
+    }
+
+    public function personalBest(): int
+    {
+        return max($this->scores);
+    }
+
+    public function personalTopThree(): array
+    {
+        $scores = $this->scores;
+        rsort($scores);
+
+        return array_slice($scores, 0, 3);
     }
 }
--- high-scores/HighScoresTest.php
+++ high-scores/HighScoresTest.php
@@ -1,99 +1,112 @@
 <?php
 
-/*
- * By adding type hints and enabling strict type checking, code can become
- * easier to read, self-documenting and reduce the number of potential bugs.
- * By default, type declarations are non-strict, which means they will attempt
- * to change the original type to match the type specified by the
- * type-declaration.
- *
- * In other words, if you pass a string to a function requiring a float,
- * it will attempt to convert the string value to a float.
- *
- * To enable strict mode, a single declare directive must be placed at the top
- * of the file.
- * This means that the strictness of typing is configured on a per-file basis.
- * This directive not only affects the type declarations of parameters, but also
- * a function's return type.
- *
- * For more info review the Concept on strict type checking in the PHP track
- * <link>.
- *
- * To disable strict typing, comment out the directive below.
- */
-
 declare(strict_types=1);
 
-class HighScoresTest extends \PHPUnit\Framework\TestCase
+namespace ExercismTest\HighScores;
+
+use Exercism\HighScores\HighScores;
+use PHPUnit\Framework\TestCase;
+
+class HighScoresTest extends TestCase
 {
     public static function setUpBeforeClass(): void
     {
         require_once 'HighScores.php';
     }
 
-    public function testListOfScores(): void
+    /**
+     * @covers \Exercism\HighScores\HighScores::scores
+     */
+    public function test_list_of_scores(): void
     {
         $input = [30, 50, 20, 70];
-        $this->assertEquals([30, 50, 20, 70], (new HighScores($input))->scores);
+        self::assertSame([30, 50, 20, 70], (new HighScores($input))->scores());
     }
 
-    public function testLatestScore(): void
+    /**
+     * @covers \Exercism\HighScores\HighScores::latest
+     */
+    public function test_latest_score(): void
     {
         $input = [100, 0, 90, 30];
-        $this->assertEquals(30, (new HighScores($input))->latest);
+        self::assertSame(30, (new HighScores($input))->latest());
     }
 
-    public function testPersonalBest(): void
+    /**
+     * @covers \Exercism\HighScores\HighScores::personalBest
+     */
+    public function test_personalBest(): void
     {
         $input = [40, 100, 70];
-        $this->assertEquals(100, (new HighScores($input))->personalBest);
+        self::assertSame(100, (new HighScores($input))->personalBest());
     }
 
-    public function testPersonalTopThreeFromAListOfScores(): void
+    /**
+     * @covers \Exercism\HighScores\HighScores::personalTopThree
+     */
+    public function test_personalTopThree_from_a_list_of_scores(): void
     {
         $input = [10, 30, 90, 30, 100, 20, 10, 0, 30, 40, 40, 70, 70];
-        $this->assertEquals([100, 90, 70], (new HighScores($input))->personalTopThree);
+        self::assertSame([100, 90, 70], (new HighScores($input))->personalTopThree());
     }
 
-    public function testPersonalTopHighestToLowest(): void
+    /**
+     * @covers \Exercism\HighScores\HighScores::personalTopThree
+     */
+    public function test_personalTopThree_highest_to_lowest(): void
     {
         $input = [20, 10, 30];
-        $this->assertEquals([30, 20, 10], (new HighScores($input))->personalTopThree);
+        self::assertSame([30, 20, 10], (new HighScores($input))->personalTopThree());
     }
 
-    public function testPersonalTopWhenThereIsATie(): void
+    /**
+     * @covers \Exercism\HighScores\HighScores::personalTopThree
+     */
+    public function test_personalTopThree_when_there_is_a_tie(): void
     {
         $input = [40, 20, 40, 30];
-        $this->assertEquals([40, 40, 30], (new HighScores($input))->personalTopThree);
+        self::assertSame([40, 40, 30], (new HighScores($input))->personalTopThree());
     }
 
-    public function testPersonalTopWhenThereAreLessThan3(): void
+    /**
+     * @covers \Exercism\HighScores\HighScores::personalTopThree
+     */
+    public function test_personalTopThree_when_there_are_less_than_3(): void
     {
         $input = [30, 70];
-        $this->assertEquals([70, 30], (new HighScores($input))->personalTopThree);
+        self::assertSame([70, 30], (new HighScores($input))->personalTopThree());
     }
 
-    public function testPersonalTopWhenThereIsOnlyOne(): void
+    /**
+     * @covers \Exercism\HighScores\HighScores::personalTopThree
+     */
+    public function test_personalTopThree_when_there_is_only_one(): void
     {
         $input = [40];
-        $this->assertEquals([40], (new HighScores($input))->personalTopThree);
+        self::assertSame([40], (new HighScores($input))->personalTopThree());
     }
 
-    public function testLatestScoreDoesNotChangeAfterGettingPersonalBest(): void
+    /**
+     * @covers \Exercism\HighScores\HighScores::latest
+     */
+    public function test_latest_score_does_not_change_after_getting_personal_best(): void
     {
         $input     = [20, 10, 30, 3, 2, 1];
         $highScore = new HighScores($input);
 
-        $this->assertEquals(30, $highScore->personalBest);
-        $this->assertEquals(1, $highScore->latest);
+        self::assertSame(30, $highScore->personalBest());
+        self::assertSame(1, $highScore->latest());
     }
 
-    public function testLatestScoreDoesNotChangeAfterGettingPersonalTopThree(): void
+    /**
+     * @covers \Exercism\HighScores\HighScores::latest
+     */
+    public function test_latest_score_does_not_change_after_getting_personalTopThree(): void
     {
         $input     = [20, 100, 30, 90, 2, 70];
         $highScore = new HighScores($input);
 
-        $this->assertEquals([100, 90, 70], $highScore->personalTopThree);
-        $this->assertEquals(70, $highScore->latest);
+        self::assertSame([100, 90, 70], $highScore->personalTopThree());
+        self::assertSame(70, $highScore->latest());
     }
 }
