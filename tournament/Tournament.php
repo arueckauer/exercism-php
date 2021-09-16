686e0b3a2e3ede61f69e0fe93a2cbe63af5e03a5
Andi Rückauer <arueckauer@gmail.com>
2021-09-16 09:35:29 +0200

    Change structure to make compatible with Exercism's infrastructure

--- tournament/Tournament.php
+++ tournament/Tournament.php
@@ -11,9 +11,6 @@ class Tournament
 
     public function __construct()
     {
-        require_once 'Team.php';
-        require_once 'TeamCollection.php';
-
         $this->teams = new TeamCollection();
     }
 
@@ -61,5 +58,92 @@ class Tournament
                 $teamB->win();
                 break;
         }
+    }
+}
+
+class TeamCollection
+{
+    /** @var Team[] */
+    private array $teams = [];
+
+    public function get(string $teamName): Team
+    {
+        if (! isset($this->teams[$teamName])) {
+            $this->teams[$teamName] = new Team($teamName);
+        }
+
+        return $this->teams[$teamName];
+    }
+
+    public function all(): array
+    {
+        $teams = $this->teams;
+
+        usort($teams, static function (Team $teamA, Team $teamB) {
+            $pointsDifference = $teamB->points() <=> $teamA->points();
+
+            if (0 !== $pointsDifference) {
+                return $pointsDifference;
+            }
+
+            return $teamA->name <=> $teamB->name;
+        });
+
+        return $teams;
+    }
+}
+
+class Team
+{
+    public string $name;
+    private int $matchesPlayed = 0;
+    private int $matchesWon    = 0;
+    private int $matchesDrawn  = 0;
+    private int $matchesLost   = 0;
+    private int $points        = 0;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function play(): void
+    {
+        ++$this->matchesPlayed;
+    }
+
+    public function win(): void
+    {
+        ++$this->matchesWon;
+        $this->points += 3;
+    }
+
+    public function draw(): void
+    {
+        ++$this->matchesDrawn;
+        ++$this->points;
+    }
+
+    public function loose(): void
+    {
+        ++$this->matchesLost;
+    }
+
+    public function points(): int
+    {
+        return $this->points;
+    }
+
+    public function toString(): string
+    {
+        return sprintf(
+            '%-30s | %2s | %2s | %2s | %2s | %2s',
+            $this->name,
+            $this->matchesPlayed,
+            $this->matchesWon,
+            $this->matchesDrawn,
+            $this->matchesLost,
+            $this->points
+        );
     }
 }
