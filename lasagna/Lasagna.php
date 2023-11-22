4b66d04af29c444928536189a1c61c10847ecfba
Andi Rückauer <arueckauer@gmail.com>
2023-11-22 21:25:01 +0100

    Adds solution for exercise lasagna

--- lasagna/Lasagna.php
+++ lasagna/Lasagna.php
@@ -4,13 +4,29 @@ declare(strict_types=1);
 
 class Lasagna
 {
-    // Please define the 'expectedCookTime()' function
+    public function expectedCookTime(): int
+    {
+        return 40;
 
-    // Please define the 'remainingCookTime($elapsed_minutes)' function
+    }
 
-    // Please define the 'totalPreparationTime($layers_to_prep)' function
+    public function remainingCookTime($elapsed_minutes): int
+    {
+        return $this->expectedCookTime() - $elapsed_minutes;
+    }
 
-    // Please define the 'totalElapsedTime($layers_to_prep, $elapsed_minutes)' function
+    public function totalPreparationTime($layers_to_prep): int
+    {
+        return $layers_to_prep * 2;
+    }
 
-    // Please define the 'alarm()' function
+    public function totalElapsedTime($layers_to_prep, $elapsed_minutes): int
+    {
+        return $this->totalPreparationTime($layers_to_prep) + $elapsed_minutes;
+    }
+
+    public function alarm(): string
+    {
+        return 'Ding!';
+    }
 }
