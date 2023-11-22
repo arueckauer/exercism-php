08398274f6382e1450a1f67f5eee827ec2fb7c97
Andi Rückauer <arueckauer@gmail.com>
2023-11-22 22:31:29 +0100

    Adds solution for exercise sweethearts

--- sweethearts/HighSchoolSweetheart.php
+++ sweethearts/HighSchoolSweetheart.php
@@ -4,23 +4,54 @@ declare(strict_types=1);
 
 class HighSchoolSweetheart
 {
+    private static string $asciiHeartTemplate = <<<EOT
+     ******       ******
+   **      **   **      **
+ **         ** **         **
+**            *            **
+**                         **
+**     %s  +  %s     **
+ **                       **
+   **                   **
+     **               **
+       **           **
+         **       **
+           **   **
+             ***
+              *
+EOT;
+
     public function firstLetter(string $name): string
     {
-        throw new BadFunctionCallException("Implement the function");
+        return substr(trim($name), 0, 1);
     }
 
     public function initial(string $name): string
     {
-        throw new BadFunctionCallException("Implement the function");
+        return strtoupper($this->firstLetter($name)) . '.';
     }
 
     public function initials(string $name): string
     {
-        throw new BadFunctionCallException("Implement the function");
+        if (str_contains($name, ' ') === false) {
+            throw new InvalidArgumentException('Name must have a space');
+        }
+
+        if (substr_count($name, ' ') > 1) {
+            throw new InvalidArgumentException('Name must have only one space');
+        }
+
+        [$firstName, $lastName] = explode(' ', $name);
+
+        return $this->initial($firstName) . ' ' . $this->initial($lastName);
     }
 
     public function pair(string $sweetheart_a, string $sweetheart_b): string
     {
-        throw new BadFunctionCallException("Implement the function");
+        return sprintf(
+            self::$asciiHeartTemplate,
+            $this->initials($sweetheart_a),
+            $this->initials($sweetheart_b),
+        );
     }
 }
