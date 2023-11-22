9ce0c034fb7b9175705811c47e8932a0079869ba
Andi Rückauer <arueckauer@gmail.com>
2023-11-22 21:46:14 +0100

    Adds argument type declarations

--- lasagna/Lasagna.php
+++ lasagna/Lasagna.php
@@ -9,17 +9,17 @@ class Lasagna
         return 40;
     }
 
-    public function remainingCookTime($elapsed_minutes): int
+    public function remainingCookTime(int $elapsed_minutes): int
     {
         return $this->expectedCookTime() - $elapsed_minutes;
     }
 
-    public function totalPreparationTime($layers_to_prep): int
+    public function totalPreparationTime(int $layers_to_prep): int
     {
         return $layers_to_prep * 2;
     }
 
-    public function totalElapsedTime($layers_to_prep, $elapsed_minutes): int
+    public function totalElapsedTime(int $layers_to_prep, int $elapsed_minutes): int
     {
         return $this->totalPreparationTime($layers_to_prep) + $elapsed_minutes;
     }
