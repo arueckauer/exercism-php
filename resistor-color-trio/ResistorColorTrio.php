4742f4d9d40ec05808393af8ceed242e9dfeb72b
Andi Rückauer <arueckauer@gmail.com>
2023-11-23 15:13:55 +0100

    Adds solution for exercise resistor-color-trio

--- resistor-color-trio/ResistorColorTrio.php
+++ resistor-color-trio/ResistorColorTrio.php
@@ -1,33 +1,67 @@
 <?php
 
-/*
- * By adding type hints and enabling strict type checking, code can become
- * easier to read, self-documenting and reduce the number of potential bugs.
- * By default, type declarations are non-strict, which means they will attempt
- * to change the original type to match the type specified by the
- * type-declaration.
- *
- * In other words, if you pass a string to a function requiring a float,
- * it will attempt to convert the string value to a float.
- *
- * To enable strict mode, a single declare directive must be placed at the top
- * of the file.
- * This means that the strictness of typing is configured on a per-file basis.
- * This directive not only affects the type declarations of parameters, but also
- * a function's return type.
- *
- * For more info review the Concept on strict type checking in the PHP track
- * <link>.
- *
- * To disable strict typing, comment out the directive below.
- */
-
 declare(strict_types=1);
 
 class ResistorColorTrio
 {
-    public function label(): string
+    public function label(array $colors): string
     {
-        throw new BadMethodCallException(sprintf('Implement the %s method', __FUNCTION__));
+        $firstNumber  = TrioResistorColorNumber::fromName($colors[0]);
+        $secondNumber = TrioResistorColorNumber::fromName($colors[1]);
+        $thirdNumber  = isset($colors[2]) ? TrioResistorColorNumber::fromName($colors[2])->value : 0;
+        $zeros        = str_pad('', $thirdNumber, '0');
+
+        return $this->format((int) ($firstNumber->value . $secondNumber->value . $zeros));
+    }
+
+    private function format(int $value): string
+    {
+        if ($value === 0) {
+            return $value . ' ohms';
+        }
+
+        if ($value % 1000000000 === 0) {
+            return $value / 1000000000 . ' gigaohms';
+        }
+
+        if ($value % 1000000 === 0) {
+            return $value / 1000000 . ' megaohms';
+        }
+
+        if ($value % 1000 === 0) {
+            return $value / 1000 . ' kiloohms';
+        }
+
+        return $value . ' ohms';
+    }
+}
+
+enum TrioResistorColorNumber: int
+{
+    case black  = 0;
+    case brown  = 1;
+    case red    = 2;
+    case orange = 3;
+    case yellow = 4;
+    case green  = 5;
+    case blue   = 6;
+    case violet = 7;
+    case grey   = 8;
+    case white  = 9;
+
+    public static function fromName(string $name): self
+    {
+        return match ($name) {
+            'black'  => self::black,
+            'brown'  => self::brown,
+            'red'    => self::red,
+            'orange' => self::orange,
+            'yellow' => self::yellow,
+            'green'  => self::green,
+            'blue'   => self::blue,
+            'violet' => self::violet,
+            'grey'   => self::grey,
+            'white'  => self::white,
+        };
     }
 }
