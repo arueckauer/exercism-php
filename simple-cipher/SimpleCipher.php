a2c8a700a8d5186e8ab84cf7fcf8d3e77e81e85f
Andi Rückauer <arueckauer@gmail.com>
2023-11-23 23:01:24 +0100

    Adds solution for exercise simple-cipher

--- simple-cipher/SimpleCipher.php
+++ simple-cipher/SimpleCipher.php
@@ -1,41 +1,78 @@
 <?php
 
-/*
- * By adding type hints and enabling strict type checking, code can become
- * easier to read, self-documenting and reduce the number of potential bugs.
- * By default, type declarations are non-strict, which means they will attempt
- * to change the original type to match the type specified by the
- * type-declaration.
- *
- * In other words, if you pass a string to a function requiring a float,
- * it will attempt to convert the string value to a float.
- *
- * To enable strict mode, a single declare directive must be placed at the top
- * of the file.
- * This means that the strictness of typing is configured on a per-file basis.
- * This directive not only affects the type declarations of parameters, but also
- * a function's return type.
- *
- * For more info review the Concept on strict type checking in the PHP track
- * <link>.
- *
- * To disable strict typing, comment out the directive below.
- */
-
 declare(strict_types=1);
 
 class SimpleCipher
 {
-    public function __construct(?string $key = null)
-    {
-        throw new BadFunctionCallException("Please implement the SimpleCipher class!");
+    private const LOWERCASE_LETTERS = 'abcdefghijklmnopqrstuvwxyz';
+    private const LETTERS           = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz';
+
+    public function __construct(
+        public ?string $key = null
+    ) {
+        if ($this->key === null) {
+            $this->key = $this->generateRandomKey();
+        }
+
+        if ($this->key === '') {
+            throw new InvalidArgumentException('Key must be at least 1 character long');
+        }
+
+        if (preg_match('/[^a-z]/', $this->key)) {
+            throw new InvalidArgumentException('Key must be lowercase letters only');
+        }
     }
 
     public function encode(string $plainText): string
     {
+        return $this->cipher($plainText, 1);
     }
 
     public function decode(string $cipherText): string
     {
+        return $this->cipher($cipherText, -1);
+    }
+
+    public function cipher(string $cipherText, int $sign): string
+    {
+        $stringLength = strlen($cipherText);
+        $keyLength    = strlen($this->key);
+        for ($i = 0; $i < $stringLength; $i++) {
+            $numberOfPlaces = strpos(self::LOWERCASE_LETTERS, $this->key[$i % $keyLength]);
+            $cipherText[$i] = $this->rotate($cipherText[$i], $sign * $numberOfPlaces);
+        }
+
+        return $cipherText;
+    }
+
+    private function rotate(string $character, int $numberOfPlaces): string
+    {
+        $numberOfPlaces %= 26;
+
+        if (! $numberOfPlaces) {
+            return $character;
+        }
+
+        if ($numberOfPlaces < 0) {
+            $numberOfPlaces += 26;
+        }
+
+        $to = substr(self::LETTERS, $numberOfPlaces * 2) . substr(self::LETTERS, 0, $numberOfPlaces * 2);
+
+        return strtr($character, self::LETTERS, $to);
+    }
+
+    private function generateRandomKey(): string
+    {
+        return substr(
+            str_shuffle(
+                str_repeat(
+                    self::LOWERCASE_LETTERS,
+                    (int) ceil(100 / strlen(self::LOWERCASE_LETTERS))
+                )
+            ),
+            1,
+            100
+        );
     }
 }
