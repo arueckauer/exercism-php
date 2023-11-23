4b647da796cdea8a6e9eecde51ba47418657d2d5
Andi Rückauer <arueckauer@gmail.com>
2023-11-23 08:43:19 +0100

    Adds solution for exercise language-list

--- language-list/LanguageList.php
+++ language-list/LanguageList.php
@@ -2,7 +2,29 @@
 
 declare(strict_types=1);
 
-function language_list()
+function language_list(string ...$language_list): array
 {
-    // implement the language list function
+    return $language_list;
+}
+
+function add_to_language_list(array $language_list, string $language): array
+{
+    $language_list[] = $language;
+    return $language_list;
+}
+
+function prune_language_list(array $language_list): array
+{
+    array_shift($language_list);
+    return $language_list;
+}
+
+function current_language(array $language_list): string
+{
+    return array_shift($language_list);
+}
+
+function language_list_length(array $language_list): int
+{
+    return count($language_list);
 }
