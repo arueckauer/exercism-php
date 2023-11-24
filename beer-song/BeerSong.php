b303feb87baba2964ce8b15736a8420e38439b12
Andi Rückauer <arueckauer@gmail.com>
2023-11-24 12:18:22 +0100

    Adds solution for exercise beer-song

--- beer-song/BeerSong.php
+++ beer-song/BeerSong.php
@@ -4,18 +4,43 @@ declare(strict_types=1);
 
 class BeerSong
 {
+    private static string $verse     = <<<VERSE
+%1\$d %2\$s of beer on the wall, %1\$d %2\$s of beer.
+Take %3\$s down and pass it around, %4\$s %5\$s of beer on the wall.
+VERSE;
+    private static string $lastVerse = <<<VERSE
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
+VERSE;
+
     public function verse(int $number): string
     {
-        throw new BadMethodCallException("Implement the verse method");
+        if ($number === 0) {
+            return static::$lastVerse;
+        }
+
+        return sprintf(
+            static::$verse,
+            $number,
+            $number === 1 ? 'bottle' : 'bottles',
+            $number === 1 ? 'it' : 'one',
+            $number === 1 ? 'no more' : $number - 1,
+            $number === 2 ? 'bottle' : 'bottles',
+        ) . "\n";
     }
 
     public function verses(int $start, int $finish): string
     {
-        throw new BadMethodCallException("Implement the verses method");
+        $verses = [];
+        foreach (range($start, $finish) as $verseNumber) {
+            $verses[] = $this->verse($verseNumber);
+        }
+
+        return implode("\n", $verses);
     }
 
     public function lyrics(): string
     {
-        throw new BadMethodCallException("Implement the lyrics method");
+        return $this->verses(99, 0);
     }
 }
