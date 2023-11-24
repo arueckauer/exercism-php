9ac53c81a54559ff3b941e2045ddcff238d8f003
Andi Rückauer <arueckauer@gmail.com>
2023-11-24 08:24:12 +0100

    Configures exercise change

--- rector.php
+++ rector.php
@@ -8,26 +8,27 @@ use Rector\Set\ValueObject\LevelSetList;
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths(
         [
-        __DIR__ . '/annalyns-infiltration',
-        __DIR__ . '/city-office',
-        __DIR__ . '/high-scores',
-        __DIR__ . '/lasagna',
-        __DIR__ . '/language-list',
-        __DIR__ . '/lucky-numbers',
-        __DIR__ . '/pizza-pi',
-        __DIR__ . '/resistor-color',
-        __DIR__ . '/reverse-string',
-        __DIR__ . '/robot-simulator',
-        __DIR__ . '/sweethearts',
-        __DIR__ . '/tournament',
-        __DIR__ . '/windowing-system',
+            __DIR__ . '/annalyns-infiltration',
+            __DIR__ . '/change',
+            __DIR__ . '/city-office',
+            __DIR__ . '/high-scores',
+            __DIR__ . '/lasagna',
+            __DIR__ . '/language-list',
+            __DIR__ . '/lucky-numbers',
+            __DIR__ . '/pizza-pi',
+            __DIR__ . '/resistor-color',
+            __DIR__ . '/reverse-string',
+            __DIR__ . '/robot-simulator',
+            __DIR__ . '/sweethearts',
+            __DIR__ . '/tournament',
+            __DIR__ . '/windowing-system',
         ]
     );
 
     $rectorConfig->sets(
         [
-        LevelSetList::UP_TO_PHP_81,
-        Rector\PHPUnit\Set\PHPUnitSetList::PHPUNIT_100,
+            LevelSetList::UP_TO_PHP_81,
+            Rector\PHPUnit\Set\PHPUnitSetList::PHPUNIT_100,
         ]
     );
 };
