2108e9f88e7375dbd719320a49436b4a7dcd17a2
Andi Rückauer <arueckauer@gmail.com>
2023-11-24 18:25:29 +0100

    Adds solution for exercise grains

--- grains/Grains.php
+++ grains/Grains.php
@@ -4,10 +4,24 @@ declare(strict_types=1);
 
 function square(int $number): string
 {
-    throw new BadFunctionCallException("Implement the square function");
+    if ($number < 1 || $number > 64) {
+        throw new InvalidArgumentException('Square must be between 1 and 64');
+    }
+
+    return sprintf('%u', squareRaw($number));
+}
+
+function squareRaw(int $number): int
+{
+    return 1 << ($number - 1);
 }
 
 function total(): string
 {
-    throw new BadFunctionCallException("Implement the total function");
+    $total = 0;
+    for ($i = 1; $i <= 64; $i++) {
+        $total += squareRaw($i);
+    }
+
+    return sprintf('%u', $total);
 }
