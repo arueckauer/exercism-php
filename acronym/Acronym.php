b0b4b229fb41608249bb13db5dbf4c5d1b07734c
Andi Rückauer <arueckauer@gmail.com>
2023-11-24 11:50:06 +0100

    Adds solution for exercise acronym

--- acronym/Acronym.php
+++ acronym/Acronym.php
@@ -4,5 +4,20 @@ declare(strict_types=1);
 
 function acronym(string $text): string
 {
-    throw new BadFunctionCallException("Implement the acronym function");
+    if (mb_substr_count($text, ' ') === 0) {
+        return '';
+    }
+
+    // Add space before uppercase character in a word
+    $text = preg_replace('/([a-z])([A-Z])/', '$1 $2', $text);
+
+    $acronym = '';
+    $words   = preg_split('/[\s-]/', mb_strtolower($text));
+    foreach ($words as $word) {
+        if (mb_strtoupper($word) !== $word) {
+            $acronym .= mb_strtoupper(mb_substr($word, 0, 1));
+        }
+    }
+
+    return $acronym;
 }
