d117feec47cf763ab21fa6e62f92695d8f41a3ec
Andi Rückauer <arueckauer@gmail.com>
2023-11-24 10:37:49 +0100

    Downloads exercise anagram

---
+++ anagram/Anagram.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Exercism\Anagram;
+
+function detectAnagrams(string $word, array $possibleAnagrams) : array
+{
+    $anagrams = [];
+    $word = mb_strtolower($word);
+    $wordValues = array_count_values(str_split($word));
+    foreach ($possibleAnagrams as $possibleAnagram) {
+        $lowerPossibleAnagram = mb_strtolower($possibleAnagram);
+
+        if ($lowerPossibleAnagram === $word) {
+            continue;
+        }
+
+        if (array_count_values(str_split($lowerPossibleAnagram)) == $wordValues) {
+            $anagrams[] = $possibleAnagram;
+        }
+    }
+
+    return $anagrams;
+}
