6c2adc80de7d06babe8b85b37d73ec3ad2574db3
Andi Rückauer <arueckauer@gmail.com>
2025-08-27 11:07:31 +0200

    Refactor coin change algorithm to use dynamic programming with recursion

--- change/Change.php
+++ change/Change.php
@@ -12,81 +12,77 @@ function findFewestCoins(array $coins, int $amount): array
         throw new InvalidArgumentException('Cannot make change for negative value');
     }
 
-    rsort($coins);
-
-    if ($coins[count($coins) - 1] > $amount) {
+    if (min($coins) > $amount) {
         throw new InvalidArgumentException('No coins small enough to make change');
     }
 
-    try {
-        $greedyChange = greedy($coins, $amount);
-    } catch (InvalidArgumentException) {
-    }
-
-    try {
-        $dynamicChange = dynamic($coins, $amount);
-    } catch (InvalidArgumentException $invalidArgumentException) {
-        if (isset($greedyChange)) {
-            return $greedyChange;
-        }
-
-        throw $invalidArgumentException;
-    }
-
-    if (! isset($greedyChange)) {
-        return $dynamicChange;
-    }
-
-    return count($greedyChange) < count($dynamicChange) ? $greedyChange : $dynamicChange;
-}
-
-function greedy(array $coins, int $amount): array
-{
-    $change = [];
-    foreach ($coins as $coin) {
-        while ($amount >= $coin) {
-            $change[] = $coin;
-            $amount  -= $coin;
-        }
-    }
-
-    if ($amount > 0) {
-        throw new InvalidArgumentException('No combination can add up to target');
-    }
-
-    sort($change);
-
-    return $change;
-}
-
-function dynamic(array $coins, int $amount): array
-{
-    $table    = [];
-    $table[0] = [];
-
+    $table = [];
     for ($i = 1; $i <= $amount; $i++) {
-        $table[$i] = null;
-        foreach ($coins as $coin) {
-            if ($coin > $i) {
-                continue;
-            }
-
-            $remainder = $i - $coin;
-            if ($table[$remainder] === null) {
-                continue;
-            }
-
-            $table[$i]   = $table[$remainder];
-            $table[$i][] = $coin;
-            break;
-        }
+        $table[$i] = findShortestCoinsPath($i, $coins, $table);
     }
 
     if ($table[$amount] === null) {
         throw new InvalidArgumentException('No combination can add up to target');
     }
 
-    sort($table[$amount]);
-
     return $table[$amount];
+}
+
+function findShortestCoinsPath(int $amount, array $coins, array &$table): ?array
+{
+    if (isset($table[$amount])) {
+        return $table[$amount];
+    }
+
+    $paths = [];
+    foreach ($coins as $coin) {
+        $remainingAmount = $amount;
+
+        if ($coin > $remainingAmount) {
+            continue;
+        }
+
+        if (isset($table[$remainingAmount])) {
+            $paths[$coin] = $table[$remainingAmount];
+            continue;
+        }
+
+        $paths[$coin][]   = $coin;
+        $table[$coin]     = $paths[$coin];
+        $remainingAmount -= $coin;
+
+        if ($remainingAmount === 0) {
+            break;
+        }
+
+        $path = findShortestCoinsPath($remainingAmount, $coins, $table);
+        if ($path === null) {
+            continue;
+        }
+
+        $paths[$coin] = [...$paths[$coin], ...$path];
+    }
+
+    if (
+        count($paths) === 1
+        && isset($paths[$amount])
+    ) {
+        return $paths[$amount];
+    }
+
+    $changeWithFewestCoins = [];
+    foreach ($paths as $path) {
+        if (
+            count($changeWithFewestCoins) === 0
+            || count($path) < count($changeWithFewestCoins)
+        ) {
+            $changeWithFewestCoins = $path;
+        }
+    }
+
+    if ($remainingAmount !== 0 && count($changeWithFewestCoins) === 0) {
+        return null;
+    }
+
+    return $changeWithFewestCoins;
 }
